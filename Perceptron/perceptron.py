4b47340e29e3d36bd4c27da8645c76ae5904d38e
domalouf <89606118+domalouf@users.noreply.github.com>
2023-11-04 13:54:30 -0600

    Made some absolutely huge progress

---
+++ Perceptron/perceptron.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def perceptron(x, y, r, T):
+    # initialize w as 0's
+
+    # for epoch 1 through T
+    #shuffle data
+    #for each example xy
+    #if wrong prediction - update w
+
+
+    return 1
